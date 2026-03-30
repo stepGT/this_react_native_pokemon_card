eb9f735d307667150307bab7fc7da141466dd6b3
Andrey Zuev <a.zuev@texrus.com>
2026-03-30 20:27:16 +1000

    Prepare Index

--- app/index.tsx
+++ app/index.tsx
@@ -1,14 +1,12 @@
-import { Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 
 export default function Index() {
-  return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-      }}>
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-    </View>
-  );
+  return <View style={styles.container}></View>;
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'f5f5f5',
+  },
+});
