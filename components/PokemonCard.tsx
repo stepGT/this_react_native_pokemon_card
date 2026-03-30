6b457a873b2b3b8a54d732b7e154cc49611a2c58
Andrey Zuev <a.zuev@texrus.com>
2026-03-30 20:48:17 +1000

    Added styles to PokemonCard component

--- components/PokemonCard.tsx
+++ components/PokemonCard.tsx
@@ -1,9 +1,30 @@
-import { Text, View } from 'react-native';
+import { Platform, StyleSheet, Text, View } from 'react-native';
 
 export default function PokemonCard() {
   return (
-    <View>
+    <View style={styles.card}>
       <Text>PokemonCard</Text>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  card: {
+    backgroundColor: 'white',
+    borderRadius: 16,
+    borderWidth: 2,
+    padding: 16,
+    margin: 16,
+    ...Platform.select({
+      ios: {
+        shadowOffset: { width: 2, height: 2 },
+        shadowColor: '#333',
+        shadowOpacity: 0.3,
+        shadowRadius: 4,
+      },
+      android: {
+        elevation: 5,
+      },
+    }),
+  },
+});
