8a08c59f9fc54eb3b256409894ffe8cd27124a05
Andrey Zuev <a.zuev@texrus.com>
2026-03-30 20:39:31 +1000

    Added component PokemonCard

--- app/index.tsx
+++ app/index.tsx
@@ -1,12 +1,18 @@
-import { StyleSheet, View } from 'react-native';
+import PokemonCard from '@/components/PokemonCard';
+import { Platform, SafeAreaView, StyleSheet } from 'react-native';
 
 export default function Index() {
-  return <View style={styles.container}></View>;
+  return (
+    <SafeAreaView style={styles.container}>
+      <PokemonCard />
+    </SafeAreaView>
+  );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: 'f5f5f5',
+    paddingTop: Platform.OS === 'android' ? 25 : 0,
   },
 });
---
+++ components/PokemonCard.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from 'react-native';
+
+export default function PokemonCard() {
+  return (
+    <View>
+      <Text>PokemonCard</Text>
+    </View>
+  );
+}
